b761111e9b4c84c799937ead4d93b39155917b31
suame2021 <suameike@yahoo.com>
2022-04-08 10:41:50 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main 
+
+ *
+
+ * Return: Always (Success)
+
+ */
+
+int main(void)
+
+{
+
+	char c;
+
+
+
+	for (c = 'a'; c <= 'z'; c++)
+
+		putchar(c);
+
+
+
+	putchar('\n');
+
+	return (0);
+
+
+
+}
